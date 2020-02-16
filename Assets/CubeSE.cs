495bd2800332610a5205051e024954c5f55f96c6
risa-nakamura <negi39390822nasi@gmail.com>
2020-02-16 14:55:33 +0900

    GoForwardUnityChan課題まで
    効果音を出す課題まで

---
+++ Assets/CubeSE.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSE : MonoBehaviour {
+
+public AudioClip sound1;
+AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+   
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "cubeTag" || collision.gameObject.tag == "groundTag")
+        {
+                audioSource.PlayOneShot(sound1);
+            
+        }
+        else if (collision.gameObject.tag == "UnityChan")
+        {
+            audioSource.volume = 0;
+        }
+
+    }
+
+}
